0f9fe89de34d2cbc803406c6fd76701b78fc5933
Parth Aggarwal <frazile.com@gmail.com>
2020-04-01 18:41:16 +0530

    providers2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:providers/counter.dart';
+import 'package:providers/pages/login/login.dart';
+import 'package:providers/providers/firebaseAuthService.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,66 +10,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // This is the parent provider of MyHomePage which works only with ChangeNotifier & is a type of Counter
-      home: ChangeNotifierProvider<Counter>(
-        create: (context) => Counter(),
-        child: MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    /* While calling `Provider.of<T>(context)` does two things
-    *  Retrives the value/object that is asked for
-    *  Registers current widget which is identified by the context as a listener
-    */
-    // `listen` param of `Provider.of<T>(context, listen: false)` is default true it can be set to false
-    final counter = Provider.of<Counter>(context, listen: false);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "You said 'I LOVE YOU' these many times:",
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            // Consumer<T> is used to register a widget as a listener to the type `Counter`
-            Consumer<Counter>(
-              builder: (_, counter, __) => Text(
-                '${counter.value}',
-                style: TextStyle(
-                  fontSize: 32.0,
-                  color: Colors.red,
-                ),
-              ),
-            ),
-          ],
+    return Provider<FirebaseAuthService>(
+      create: (_) => FirebaseAuthService(),
+      child: MaterialApp(
+        title: 'Login',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counter.increment,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        home: Login(),
       ),
     );
   }
